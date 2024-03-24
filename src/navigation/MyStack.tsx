f26ddbd4072fdb645a29fe2b50d30ff7a3ea15b6
sude.okur <71027708+sudeokuur@users.noreply.github.com>
2024-03-24 20:06:09 +0300

    tabs bug fix

--- src/navigation/MyStack.tsx
+++ src/navigation/MyStack.tsx
@@ -1,28 +1,25 @@
-
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import EventDetailScreen from '../screens/EventDetailScreen';
-import HomePage from "../screens/HomePage";
 import LetsGetStarted from '../screens/LetsGetStarted';
 import LoginScreen from "../screens/LoginScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import SearchScreen from "../screens/SearchScreen";
 import SignUpScreen from "../screens/SignUpScreen";
+import Tabs from './Tabs'; // Import your bottom tab navigator component
+
 const Stack = createStackNavigator();
 
 const MyStack: React.FC = () => {
   return (
     <Stack.Navigator  screenOptions={{headerShown: false}}>
-      <Stack.Screen name="HomePage" component={HomePage}/>
+      {/* You can remove the individual screen components and replace them with Tabs */}
+      <Stack.Screen name="Tabs" component={Tabs} />
+      <Stack.Screen name="EventDetail" component={EventDetailScreen}/>      
+      {/* You can keep other screens as needed */}
       <Stack.Screen name="LetsGetStarted" component={LetsGetStarted}/>
-      <Stack.Screen name="Search" component={SearchScreen}/>
       <Stack.Screen name="Login" component={LoginScreen}/>
       <Stack.Screen name="SignUp" component={SignUpScreen}/>
-      <Stack.Screen name="Profile" component={ProfileScreen}/>
-      <Stack.Screen name="EventDetail" component={EventDetailScreen}/>      
-
     </Stack.Navigator>
   );
 }
 
-export default MyStack; 
+export default MyStack;

e8d86c47986376d61bdff396d9ec9bdbe794640a
sude.okur <71027708+sudeokuur@users.noreply.github.com>
2024-05-25 23:37:14 +0300

    commit for latest version

---
+++ src/navigation/MyStack.tsx
@@ -0,0 +1,31 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import EventDetailScreen from '../screens/EventDetailScreen';
+import LetsGetStarted from '../screens/LetsGetStarted';
+import LoginScreen from "../screens/LoginScreen";
+import SelectedEventScreen from '../screens/SelectedEventScreen';
+import SignUpScreen from "../screens/SignUpScreen";
+import Tabs from './Tabs'; // Import your bottom tab navigator component
+
+const Stack = createStackNavigator();
+
+const MyStack: React.FC = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      {}
+      <Stack.Screen name="Tabs" component={Tabs} />
+
+
+      {}
+      <Stack.Screen name="LetsGetStarted" component={LetsGetStarted}/>
+      <Stack.Screen name="SelectedEventScreen" component={SelectedEventScreen}/>
+      <Stack.Screen name="EventDetail" component={EventDetailScreen}/>      
+      {}
+      <Stack.Screen name="Login" component={LoginScreen}/>
+      <Stack.Screen name="SignUp" component={SignUpScreen}/>
+
+    </Stack.Navigator>
+  );
+}
+
+export default MyStack;
